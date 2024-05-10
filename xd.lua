2f1d2225a35b51c08aa42da64b12e1bb9abbc28b
onion <131298519+onion1242w@users.noreply.github.com>
2024-05-10 16:35:31 +0300

    Update xd.lua

--- xd.lua
+++ xd.lua
@@ -14,6 +14,10 @@ local MainButton = Category:Button("Main", "http://www.roblox.com/asset/?id=8395
 
 local MainTab = MainButton:Section("Main", "Left")
 
+-- // Config \\ --
+
+local CurrentFruitModel = nil
+
 -- // Functions \\ --
 
 local function SpawnShips(Count : number)
@@ -23,6 +27,51 @@ local function SpawnShips(Count : number)
             task.wait(0.4)
         end
     end)
+end
+
+local function GodMode()
+    if Player.Character then
+        Player.Character.Humanoid.MaxHealth = math.huge
+        Player.Character.Humanoid.Health = math.huge
+    end
+end
+
+local function FindFruitInInventory(FruitName : string)
+    local Fru = Player.Backpack:FindFirstChild(FruitName) or Player.Character:FindFirstChild(FruitName)
+    return Fru
+end
+
+local function FindEquippedFruit()
+    for i, v in pairs(workspace:GetChildren()) do
+        if v:FindFirstChild("NameVal") then
+            local FoundFruit = FindFruitInInventory(v.NameVal.Value)
+            if FoundFruit then
+                return v -- Fruit found, return it and break loop
+            end
+        end
+    end
+    return nil -- nothing found return nil
+end
+
+local function LoopTargetYield(TarChar : Model, TimeToTarget : number)
+    local TimeC = tick() + TimeToTarget
+    repeat
+        if Player.Character and TarChar and CurrentFruitModel then
+            if TarChar:FindFirstChild("Torso") then
+                local BackPos = TarChar.Torso.Position + (TarChar.Torso.CFrame.LookVector * 0.8)
+                Player.Character:PivotTo(CFrame.new(BackPos, TarChar.Torso.Position))
+            end
+        end
+        task.wait()
+    until (tick() > TimeC)
+end
+
+local function LoopAttack(TimeToTarget : number)
+    local TimeC = tick() + TimeToTarget
+    while tick() < TimeC and Player.Character and CurrentFruitModel do
+        CurrentFruitModel.ten:FireServer()
+        task.wait(0.3)
+    end
 end
 
 -- // UI \\ --
@@ -46,7 +95,51 @@ MainTab:Button({
     }, 
     function(v)
         local ShipVal = sliderVal:getValue()
-        print("spawnnship")
         SpawnShips(ShipVal)
     end
 )
+
+MainTab:Button({
+        Title = "GodMode",
+        ButtonName = "Enable",
+        Description = "",
+    }, 
+    function(v)
+        GodMode()
+    end
+)
+
+local AFarmVal = MainTab:Toggle({
+        Title = "Auto Farm Players",
+        Description = "",
+        Default = false
+    },
+    function(v)
+        -- Nothing
+    end
+)
+
+-- // Loops \\ --
+
+task.spawn(function()
+    while task.wait() do
+        -- Fruit Selected
+        if Player.Character then
+            CurrentFruitModel = FindEquippedFruit()
+        end
+        -- AutoFarm
+        if AFarmVal:getValue() and Player.Character then
+            for i, v in pairs(Players:GetPlayers()) do
+                if not (v == Player) and AFarmVal:getValue() then
+                    local CharPlr = v.Character
+                    if CharPlr then
+                        local TimeToTarget = 1.3
+                        task.spawn(LoopTargetYield, CharPlr, TimeToTarget)
+                        task.spawn(LoopAttack, Title)
+                        task.wait(TimeToTarget)
+                    end
+                end
+            end
+        end
+    end
+end)
