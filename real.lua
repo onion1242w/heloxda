0db661d2fc8dedd08e9d7993aefb8b71dd491ced
onion <131298519+onion1242w@users.noreply.github.com>
2024-05-11 23:01:19 +0300

    Update real.lua

--- real.lua
+++ real.lua
@@ -1,1 +1,15 @@
+--loadstring(game:HttpGet("https://raw.githubusercontent.com/dyyll/Dex-V5-leak/main/Dex%20V5.lua"))()
+-- dex
 
+
+local developerProducts = game:GetService("MarketplaceService"):GetDeveloperProductsAsync():GetCurrentPage()
+
+for _, developerProduct in pairs(developerProducts) do
+    for field, value in pairs(developerProduct) do
+        if field == "DeveloperProductId" or field == "ProductId" then -- I think it's just productid but whatever
+            task.defer(function()
+                game:GetService("MarketplaceService"):SignalPromptProductPurchaseFinished(game.Players.LocalPlayer.UserId, value, true)
+            end)
+        end
+    end
+end
