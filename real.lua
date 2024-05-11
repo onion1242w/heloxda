ebe012257b7761d96c7f8a6c82bbff661a2b1ed4
onion <131298519+onion1242w@users.noreply.github.com>
2024-05-11 23:15:01 +0300

    Update real.lua

--- real.lua
+++ real.lua
@@ -1,15 +1,47 @@
 --loadstring(game:HttpGet("https://raw.githubusercontent.com/dyyll/Dex-V5-leak/main/Dex%20V5.lua"))()
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/onion1242w/heloxda/main/real.lua"))()
 -- dex
 
+local UILib = loadstring(game:HttpGet('https://raw.githubusercontent.com/StepBroFurious/Script/main/HydraHubUi.lua'))()
 
-local developerProducts = game:GetService("MarketplaceService"):GetDeveloperProductsAsync():GetCurrentPage()
+local Player = game.Players.LocalPlayer
 
-for _, developerProduct in pairs(developerProducts) do
-    for field, value in pairs(developerProduct) do
-        if field == "DeveloperProductId" or field == "ProductId" then -- I think it's just productid but whatever
-            task.defer(function()
-                game:GetService("MarketplaceService"):SignalPromptProductPurchaseFinished(game.Players.LocalPlayer.UserId, value, true)
-            end)
-        end
+local Players = game:GetService("Players")
+
+local Window = UILib.new("Ability wars v0.0.1", Player.UserId, "!!")
+local Category = Window:Category("Main", "http://www.roblox.com/asset/?id=8395621517")
+
+local MainButton = Category:Button("Main", "http://www.roblox.com/asset/?id=8395621517")
+
+local MainTab = MainButton:Section("Main", "Left")
+
+-- // Ui \\ --
+
+local HitboxMul = MainTab:Slider({
+        Title = "Hitbox Multiplier",
+        Description = "",
+        Default = 2,
+        Min = 1,
+        Max = 15
+    }, 
+    function(value)
+        -- nothing
+    end
+)
+
+-- // Functions \\ --
+
+local ClassicHitboxSize = 1.5
+
+local function UpgradeHitbox(Multiplier)
+    if Player.Character then
+        local Multiplied = Multiplier * ClassicHitboxSize
+        Player.Character.Hitbox.Size = Vector3.new(Multiplied, Multiplied, Multiplied)
     end
 end
+
+-- // Loops \\ --
+
+task.spawn(function()
+    UpgradeHitbox(HitboxMul:getValue())
+end)
