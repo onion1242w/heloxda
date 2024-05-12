2c1e0b14a037946605601289971bef3c5bdf3326
onion <131298519+onion1242w@users.noreply.github.com>
2024-05-12 13:55:35 +0300

    Update real.lua

--- real.lua
+++ real.lua
@@ -1,86 +1,18 @@
---loadstring(game:HttpGet("https://raw.githubusercontent.com/dyyll/Dex-V5-leak/main/Dex%20V5.lua"))()
--- loadstring(game:HttpGet("https://raw.githubusercontent.com/onion1242w/heloxda/main/real.lua"))()
--- dex
+local Players = game.Players
+local Player = Players.LocalPlayer
 
-local UILib = loadstring(game:HttpGet('https://raw.githubusercontent.com/StepBroFurious/Script/main/HydraHubUi.lua'))()
-
-local Player = game.Players.LocalPlayer
-
-local Players = game:GetService("Players")
-
-local Window = UILib.new("Ability wars v0.0.1", Player.UserId, "!!")
-local Category = Window:Category("Main", "http://www.roblox.com/asset/?id=8395621517")
-
-local MainButton = Category:Button("Main", "http://www.roblox.com/asset/?id=8395621517")
-
-local MainTab = MainButton:Section("Main", "Left")
-
-local RemoteEvents = game:GetService("ReplicatedStorage"):WaitForChild("Remote Events")
-
--- // Ui \\ --
-
-local HitboxMul = MainTab:Slider({
-        Title = "Hitbox Multiplier",
-        Description = "",
-        Default = 2,
-        Min = 1,
-        Max = 15
-    }, 
-    function(value)
-        -- nothing
-    end
-)
-
-local AFarmVal = MainTab:Toggle({
-        Title = "Auto Farm Players",
-        Description = "",
-        Default = false
-    },
-    function(v)
-        -- Nothing
-    end
-)
-
--- // Functions \\ --
-
-local ClassicHitboxSize = 1.5
-
-local function UpgradeHitbox(Multiplier)
+local function KillTarget(TargetModel : Model)
     if Player.Character then
-        local Multiplied = Multiplier * ClassicHitboxSize
-        Player.Character.Hitbox.Size = Vector3.new(Multiplied, Multiplied, Multiplied)
+        Player.Character.USP.GunScript_Server.InflictTarget:FireServer("Torso", TargetModel:WaitForChild("Humanoid"), TargetModel.Torso, Player.Character.USP, Vector3.new(-0.13287943601608276, -0.226749986410141, -0.9648458957672119))
     end
 end
 
-local function PunchHim(TargetChar)
-    if TargetChar:FindFirstChild("Torso") then
-        local args = {
-            [1] = TargetChar, -- Target Model
-            [2] = TargetChar:GetPivot().Position, -- Efect position
-            [3] = 12.274284362792969, -- idk whats this (maybe hitbox size???)
-            [4] = TargetChar.Torso -- Target Part
-        }
-        RemoteEvents.Punch:FireServer(unpack(args))
-    end
-end
-
--- // Loops \\ --
-
-task.spawn(function()
-    while task.wait() do
-        UpgradeHitbox(HitboxMul:getValue())
-    end
-end)
-
-task.spawn(function()
-    while task.wait(0.2) do
-        if AFarmVal:getValue() then
-            for i, v in pairs(Players:GetChildren()) do
-                if v.Character then
-                    PunchHim(v.Character)
-                    task.wait(0.05)
-                end
-            end
+local function KillAll()
+    for i, v in pairs(Players:GetChildren()) do
+        if v.Character then
+            KillTarget(v.Character)
         end
     end
-end)
+end
+
+KillAll()
